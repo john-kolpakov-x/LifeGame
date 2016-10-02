9677793dff23b484650e6e3b3f2423d02244d83e
pompei <john.kolpakov.x@gmail.com>
2016-10-02 15:32:38 +0600

    Started init

---
+++ src/wow/Asd.java
@@ -0,0 +1,7 @@
+package wow;
+
+public class Asd {
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
+}
---
+++ test_src/wow/AsdTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class AsdTest {
+
+  @Test
+  public void asd() throws Exception {
+    assertThat(1).isEqualTo(1);
+  }
+}
